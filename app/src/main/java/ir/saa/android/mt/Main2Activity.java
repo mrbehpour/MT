97d06fdda2a8bda9a3952ced6eb65c208a1fd854
m.aref <aref.m62@live.com>
2018-07-14 16:49:52 +0430

    این تغییرات مربوط به فرم ورود بنام al_login هست همانطور که تلفنی صحبت کردیم عکس پس زمینه و لوگو رو بصورت گیف متحرک براتون میفرستم کد نویسی میتونه انجام بشه چون آی دی ها طبق همان فرمول و روشی که گفتید نام گذاری شدند ممنون

--- app/src/main/java/ir/saa/android/mt/Main2Activity.java
+++ app/src/main/java/ir/saa/android/mt/Main2Activity.java
@@ -1,38 +1,68 @@
 package ir.saa.android.mt;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.VideoView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main2Activity extends AppCompatActivity {
-
+    private VideoView vvLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.al_login);
 
-        // you need to have a list of data that you want the spinner to display
-        List<String> spinnerArray =  new ArrayList<>();
-        spinnerArray.add("علی علوی");
-        spinnerArray.add("مجید مجیدی");
-        spinnerArray.add("علی کاشانی راد");
-        spinnerArray.add("میثم محمدی");
-        spinnerArray.add("محمد مهدی صادقی عارف");
-        spinnerArray.add("علیرضا خسروی");
-        spinnerArray.add("مهران مدیری");
-        spinnerArray.add("علی صادقی");
-        spinnerArray.add("مجید محمدی نیا");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, R.layout.al_spinner_item, spinnerArray);
-        adapter.setDropDownViewResource(R.layout.al_spinner_dropdown_item);
-        Spinner sItems = findViewById(R.id.spnUserName);
-        sItems.setAdapter(adapter);
+    //Login
+    // you need to have a list of data that you want the spinner to display
+    List<String> spinnerArray =  new ArrayList<>();
+    spinnerArray.add("علی علوی");
+    spinnerArray.add("مجید مجیدی");
+    spinnerArray.add("علی کاشانی راد");
+    spinnerArray.add("میثم محمدی");
+    spinnerArray.add("محمد مهدی صادقی عارف");
+    spinnerArray.add("علیرضا خسروی");
+    spinnerArray.add("مهران مدیری");
+    spinnerArray.add("علی صادقی");
+    spinnerArray.add("مجید محمدی نیا");
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(
+            this, R.layout.al_spinner_item, spinnerArray);
+    adapter.setDropDownViewResource(R.layout.al_spinner_dropdown_item);
+    Spinner sItems = findViewById(R.id.spnUserName);
+    sItems.setAdapter(adapter);
+
+    //set video background
+    vvLogin = (VideoView) findViewById(R.id.vvLogin);
+    Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.login);
+
+    vvLogin.setVideoURI(uri);
+    vvLogin.start();
+
+    vvLogin.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mediaPlayer) {
+            mediaPlayer.setLooping(true);
+        }
+    });
+    }
+
+
+//        Bazdid
+
+
+
 
     }
-}
+
+
