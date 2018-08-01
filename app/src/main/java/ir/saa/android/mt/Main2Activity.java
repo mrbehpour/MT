bb16d71252ebbccf24542b785f2d195fad42cd98
m.aref <aref.m62@live.com>
2018-08-01 14:57:51 +0430

    تغییرات جدید در تاریخ 97.05.10

--- app/src/main/java/ir/saa/android/mt/Main2Activity.java
+++ app/src/main/java/ir/saa/android/mt/Main2Activity.java
@@ -12,26 +12,26 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.al_login);
+        setContentView(R.layout.al_moshtarak);
 
-        //Login
-        // you need to have a list of data that you want the spinner to display
-        List<String> spinnerArray =  new ArrayList<>();
-        spinnerArray.add("علی علوی");
-        spinnerArray.add("مجید مجیدی");
-        spinnerArray.add("علی کاشانی راد");
-        spinnerArray.add("میثم محمدی");
-        spinnerArray.add("محمد مهدی صادقی عارف");
-        spinnerArray.add("علیرضا خسروی");
-        spinnerArray.add("مهران مدیری");
-        spinnerArray.add("علی صادقی");
-        spinnerArray.add("مجید محمدی نیا");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, R.layout.al_spinner_item, spinnerArray);
-        adapter.setDropDownViewResource(R.layout.al_spinner_dropdown_item);
-        Spinner sItems = findViewById(R.id.spnUserName);
-        sItems.setAdapter(adapter);
+//        //Login
+//        // you need to have a list of data that you want the spinner to display
+//        List<String> spinnerArray =  new ArrayList<>();
+//        spinnerArray.add("علی علوی");
+//        spinnerArray.add("مجید مجیدی");
+//        spinnerArray.add("علی کاشانی راد");
+//        spinnerArray.add("میثم محمدی");
+//        spinnerArray.add("محمد مهدی صادقی عارف");
+//        spinnerArray.add("علیرضا خسروی");
+//        spinnerArray.add("مهران مدیری");
+//        spinnerArray.add("علی صادقی");
+//        spinnerArray.add("مجید محمدی نیا");
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                this, R.layout.al_spinner_item, spinnerArray);
+//        adapter.setDropDownViewResource(R.layout.al_spinner_dropdown_item);
+//        Spinner sItems = findViewById(R.id.spnUserName);
+//        sItems.setAdapter(adapter);
 
 
 
