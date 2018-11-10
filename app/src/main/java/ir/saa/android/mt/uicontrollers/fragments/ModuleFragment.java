976efb793b33176c2ebd3c9ca1074284dbcf917a
Mohamadi <Hamid.mohammadi66@outlook.com>
2018-11-10 08:58:02 +0330

    Merge branch 'develop' of https://saadeveloper.visualstudio.com/MT_NEW/_git/MTNEW into develop
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/res/layout/fragment_polomp_save.xml
    #	app/src/main/res/layout/fragment_test_contor.xml

--- app/src/main/java/ir/saa/android/mt/uicontrollers/fragments/ModuleFragment.java
+++ app/src/main/java/ir/saa/android/mt/uicontrollers/fragments/ModuleFragment.java
@@ -1,16 +1,21 @@
 package ir.saa.android.mt.uicontrollers.fragments;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ir.saa.android.mt.R;
@@ -22,7 +27,8 @@ public class ModuleFragment extends Fragment
 {
     ModuleViewModel moduleViewModel;
     Spinner spinner;
-
+    SwitchCompat swBluetooth;
+    List<String> spinnerArray;
     public ModuleFragment() {
     }
 
@@ -36,8 +42,39 @@ public class ModuleFragment extends Fragment
         View rootView = inflater.inflate(R.layout.fragment_module, container, false);
 
         moduleViewModel = ViewModelProviders.of(getActivity()).get(ModuleViewModel.class);
+        swBluetooth=rootView.findViewById(R.id.swBluetooth);
+        spinnerArray=new ArrayList<>();
         spinner = rootView.findViewById(R.id.spnPaired);
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        moduleViewModel.listBluetoothName.observe(this, new Observer<List<String>>() {
+            @Override
+            public void onChanged(@Nullable List<String> strings) {
+                spinnerArray=strings;
+                if(strings.size()!=0) {
+                    fillPairedDeviceSpinner();
+                }
+            }
+        });
+        moduleViewModel.bluetoothIsEnable().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                swBluetooth.setChecked(aBoolean);
+            }
+        });
+
+        swBluetooth.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    moduleViewModel.setBluetoothEnable(true);
+
+                }else{
+                    moduleViewModel.setBluetoothEnable(false);
+
+
+                }
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
         {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -52,12 +89,12 @@ public class ModuleFragment extends Fragment
             }
         });
 
-        fillPairedDeviceSpinner();
+
         return rootView;
     }
 
     private void fillPairedDeviceSpinner(){
-        List<String> spinnerArray = moduleViewModel.getPairedDevice();
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(), R.layout.al_majol_spinner_item, spinnerArray);
 
--- app/src/main/java/ir/saa/android/mt/viewmodels/ModuleViewModel.java
+++ app/src/main/java/ir/saa/android/mt/viewmodels/ModuleViewModel.java
@@ -2,8 +2,12 @@ package ir.saa.android.mt.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.bluetooth.BluetoothAdapter;
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ir.saa.android.mt.repositories.bluetooth.Bluetooth;
@@ -11,12 +15,37 @@ import ir.saa.android.mt.repositories.bluetooth.Bluetooth;
 public class ModuleViewModel extends AndroidViewModel {
 
     private Bluetooth bluetooth;
+    private BluetoothAdapter bluetoothAdapter;
+    private MutableLiveData<Boolean> isEnableBluetooth;
+    public MutableLiveData<List<String>> listBluetoothName;
     public ModuleViewModel(@NonNull Application application) {
         super(application);
         bluetooth = Bluetooth.getInstance();
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if(isEnableBluetooth==null)
+            isEnableBluetooth=new MutableLiveData<>();
+        if(listBluetoothName==null)
+            listBluetoothName=new MutableLiveData<>();
+
     }
 
-    public List<String> getPairedDevice(){
+    private List<String> getPairedDevice(){
         return bluetooth.getPairedDevice();
+    }
+
+    public LiveData<Boolean> bluetoothIsEnable(){
+        final BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        isEnableBluetooth.postValue(bluetoothAdapter.isEnabled());
+        return isEnableBluetooth;
+    }
+
+    public void setBluetoothEnable(Boolean state){
+        if(state){
+            bluetoothAdapter.enable();
+            listBluetoothName.postValue(getPairedDevice());
+        }else{
+            bluetoothAdapter.disable();
+            listBluetoothName.postValue(new ArrayList<String>());
+        }
     }
 }
