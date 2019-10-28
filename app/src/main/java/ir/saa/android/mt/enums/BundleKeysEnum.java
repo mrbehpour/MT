aacdbba8708b3d0146bea712c33e9d27c31f9bed
Behpour <mrbehpour@outlook.com>
2019-10-28 09:20:01 +0330

    9800806

--- app/src/main/java/ir/saa/android/mt/enums/BundleKeysEnum.java
+++ app/src/main/java/ir/saa/android/mt/enums/BundleKeysEnum.java
@@ -1,11 +1,1 @@
-package ir.saa.android.mt.enums;
-
-/**
- * Created by h.eskandari on 5/21/2018.
- */
-
-public class BundleKeysEnum {
-    public static final String ClientID = "ClientID";
-    public static final String ClassPolompParams = "ClassPolompParams";
-    public static final String TestContorParams = "TestContorParams";
-}
+package ir.saa.android.mt.enums;/** * Created by h.eskandari on 5/21/2018. */public class BundleKeysEnum {    public static final String ClientID = "ClientID";    public static final String ClassPolompParams = "ClassPolompParams";    public static final String TestContorParams = "TestContorParams";}
--- app/src/main/java/ir/saa/android/mt/enums/SharePrefEnum.java
+++ app/src/main/java/ir/saa/android/mt/enums/SharePrefEnum.java
@@ -1,14 +1,1 @@
-package ir.saa.android.mt.enums;
-
-public class SharePrefEnum {
-    public static final String ModuleBluetoothName = "ModuleBluetoothName";
-    public static final String ModuleBluetoothNameRead = "ModuleBluetoothNameRead";
-    public static final String FontSize = "FontSize";
-    public static final String AddressServer = "AddressServer";
-    public static final String DeviceId ="DeviceId";
-    public static final String RegionId ="RegionId";
-    public static final String RegionName ="RegionName";
-    public static final String EmpId ="EmpId";
-    public static final String EmpName ="EmpName";
-    public static final String CorrectCoeff = "CorrectCoeff";
-}
+package ir.saa.android.mt.enums;public class SharePrefEnum {    public static final String ModuleBluetoothName = "ModuleBluetoothName";    public static final String ModuleBluetoothNameRead = "ModuleBluetoothNameRead";    public static final String FontSize = "FontSize";    public static final String AddressServer = "AddressServer";    public static final String DeviceId ="DeviceId";    public static final String RegionId ="RegionId";    public static final String RegionName ="RegionName";    public static final String EmpId ="EmpId";    public static final String EmpName ="EmpName";    public static final String CorrectCoeff = "CorrectCoeff";}
